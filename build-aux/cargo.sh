e66493f471eaeea9590ba50a4c54642fa76d8fcb
Felix Häcker <haeckerfelix@gnome.org>
2019-07-27 13:55:31 +0200

    Fix #385

--- build-aux/cargo.sh
+++ build-aux/cargo.sh
@@ -2,19 +2,21 @@
 
 export MESON_BUILD_ROOT="$1"
 export MESON_SOURCE_ROOT="$2"
+export SHORTWAVE_OUTPUT="$3"
+export SHORTWAVE_LOCALEDIR="$4"
+export SHORTWAVE_PROFILE="$5"
 export CARGO_TARGET_DIR="$MESON_BUILD_ROOT"/target
 export CARGO_HOME="$CARGO_TARGET_DIR"/cargo-home
-export SHORTWAVE_LOCALEDIR="$4"
 
-if [[ $5 = "Devel" ]]
+if test "$SHORTWAVE_PROFILE" != "Devel"
 then
-    echo "DEBUG MODE"
-    cargo build --manifest-path \
-        "$MESON_SOURCE_ROOT"/Cargo.toml && \
-        cp "$CARGO_TARGET_DIR"/debug/shortwave $3
-else
-    echo "RELEASE MODE"
+    echo "** RELEASE MODE **"
     cargo build --manifest-path \
         "$MESON_SOURCE_ROOT"/Cargo.toml --release && \
-        cp "$CARGO_TARGET_DIR"/release/shortwave $3
+        cp "$CARGO_TARGET_DIR"/release/shortwave $SHORTWAVE_OUTPUT
+else
+    echo "** DEBUG MODE **"
+    cargo build --manifest-path \
+        "$MESON_SOURCE_ROOT"/Cargo.toml && \
+        cp "$CARGO_TARGET_DIR"/debug/shortwave $SHORTWAVE_OUTPUT
 fi
