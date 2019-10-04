d466af77615bf5f376bcd5dc834272560d09e884
Felix Häcker <haeckerfelix@gnome.org>
2019-10-04 22:21:22 +0200

    Make basic discover page implementation

--- src/discover/pages/discover.rs
+++ src/discover/pages/discover.rs
@@ -13,9 +13,11 @@ use crate::settings::{Key, SettingsManager};
 
 pub struct Discover {
     pub widget: gtk::Box,
-
     client: Client,
-    flowbox: Rc<StationFlowBox>,
+
+    votes_flowbox: Rc<StationFlowBox>,
+    trending_flowbox: Rc<StationFlowBox>,
+    clicked_flowbox: Rc<StationFlowBox>,
 
     builder: gtk::Builder,
     sender: Sender<Action>,
@@ -28,21 +30,71 @@ impl Discover {
 
         let client = Client::new(Url::parse(&SettingsManager::get_string(Key::ApiServer)).unwrap());
 
-        let flowbox = Rc::new(StationFlowBox::new(sender.clone()));
+        get_widget!(builder, gtk::Box, votes_box);
+        let votes_flowbox = Rc::new(StationFlowBox::new(sender.clone()));
+        votes_box.add(&votes_flowbox.widget);
+
+        get_widget!(builder, gtk::Box, trending_box);
+        let trending_flowbox = Rc::new(StationFlowBox::new(sender.clone()));
+        trending_box.add(&trending_flowbox.widget);
+
+        get_widget!(builder, gtk::Box, clicked_box);
+        let clicked_flowbox = Rc::new(StationFlowBox::new(sender.clone()));
+        clicked_box.add(&clicked_flowbox.widget);
 
         let search = Self {
             widget: discover,
             client,
-            flowbox,
+            votes_flowbox,
+            trending_flowbox,
+            clicked_flowbox,
             builder,
             sender,
         };
 
-        search.setup_signals();
+        search.fetch_data();
         search
     }
 
-    fn setup_signals(&self) {
+    fn fetch_data(&self){
+        // Stations with the most votes
+        let mut votes_request = StationRequest::default();
+        votes_request.order = Some("votes".to_string());
+        votes_request.limit = Some(12);
+        votes_request.reverse = Some(true);
+        self.fill_flowbox(self.votes_flowbox.clone(), votes_request);
 
+        // Stations with the highest clicktrend
+        let mut trending_request = StationRequest::default();
+        trending_request.order = Some("clicktrend".to_string());
+        trending_request.limit = Some(12);
+        self.fill_flowbox(self.trending_flowbox.clone(), trending_request);
+
+        // Stations which got recently clicked
+        let mut clicked_request = StationRequest::default();
+        clicked_request.order = Some("clicktimestamp".to_string());
+        clicked_request.limit = Some(12);
+        self.fill_flowbox(self.clicked_flowbox.clone(), clicked_request);
+    }
+
+    fn fill_flowbox(&self, fb: Rc<StationFlowBox>, request: StationRequest){
+        let client = self.client.clone();
+        let flowbox = fb.clone();
+        let sender = self.sender.clone();
+        let fut = client.send_station_request(request).map(move |stations| {
+            match stations{
+                Ok(s) => {
+                    flowbox.clear();
+                    flowbox.add_stations(s);
+                },
+                Err(err) => {
+                    let notification = Notification::new_error("Could not receive station data.", &err.to_string());
+                    sender.send(Action::ViewShowNotification(notification.clone())).unwrap();
+                }
+            }
+        });
+
+        let ctx = glib::MainContext::default();
+        ctx.spawn_local(fut);
     }
 }
